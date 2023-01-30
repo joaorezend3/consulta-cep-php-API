b2848f0bd96fc6855c2ee7e4ab388313a73a8d4f
joaoerzend3 <joaovitorezende45@gmail.com>
2023-01-30 20:17:27 -0300

    ready🚀

--- php/viacep.php
+++ php/viacep.php
@@ -1,6 +1,4 @@
 <?php
-
-/* passando o endereço > objeto*/
 $address = (object)[
     'cep' => "",
     'logradouro' => "",
@@ -8,20 +6,8 @@ $address = (object)[
     'uf' => "",
 
 ];
-/* se o endereço existe, cira a função */
 if (isset($_POST['cep'])) {
     $cep = $_POST['cep'];
-
-
-    /* caso o form seja pre preenchido com algum caractere
-    $cep = str_replace('_', '', $cep);
-    $cep = str_replace('+', '', $cep);
-    $cep = str_replace(' ', '', $cep);
-    $cep = str_replace('.', '', $cep);
-    */
-
-
     $url = "https://viacep.com.br/ws/{$cep}/json/";
-
     $address = json_decode(file_get_contents($url));
 }
